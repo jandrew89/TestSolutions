fc96f68a9dd19d4adc326eaf64bbe33ba65b3665
Jason Christman <jc_84moss@hotmail.com>
2017-02-07 12:10:42 -0700

    REST Web Api Dependency Injection
    Structure Map for DI.

---
+++ TestSolutions.Service/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TestSolutions.Application.Customers;
+using TestSolutions.Application.Customers.Queries.Abstractions;
+
+namespace TestSolutions.Service.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        private readonly IGetCustomersQuery _query;
+        public CustomersController(IGetCustomersQuery query)
+        {
+            _query = query;
+        }
+
+        public IEnumerable<CustomerModel> Get()
+        {
+            return _query.GetCustomersList();
+        }
+    }
+}
