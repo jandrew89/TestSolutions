c74c6e7ffd50e605bcfc2782846c77431190863d
Jason Christman <jc_84moss@hotmail.com>
2017-02-08 12:13:48 -0700

    Added Date support

---
+++ TestSolutions.Common/Dates/IDateService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSolutions.Common.Dates
+{
+    public interface IDateService
+    {
+        DateTime GetDate();
+    }
+}
