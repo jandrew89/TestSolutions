893455b1c3492e2ae8f6dc3d7055ce1e45c2d638
Jason Christman <jc_84moss@hotmail.com>
2017-03-02 15:40:17 -0700

    Event Args Depreciated

---
+++ TestSolutions.Domain/DomainEvents/CustomerEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestSolutions.Domain.Customers;
+
+namespace TestSolutions.Domain.DomainEvents
+{
+    public class CustomerEventArgs : EventArgs
+    {
+        private Customer _customer;
+
+        public CustomerEventArgs(Customer customer)
+        {
+            this._customer = customer;
+        }
+
+        public Customer Customer { get { return _customer; } }
+    }
+}
