4bfed581659df9db86e79e4c26c399cc2c13b9d2
Jason Christman <jc_84moss@hotmail.com>
2017-03-02 15:41:41 -0700

    RabbitMQ Messaging

--- TestSolutions.Service/Global.asax.cs
+++ TestSolutions.Service/Global.asax.cs
@@ -5,13 +5,15 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using TestSolutions.Common.ApplicationSettings;
+using TestSolutions.Common.DomainEvents;
 using TestSolutions.Service.App_Start;
 
 namespace TestSolutions.Service
 {
     public class WebApiApplication : HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
